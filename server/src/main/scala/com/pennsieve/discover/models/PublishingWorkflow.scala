244b9eaa269bedd8cfbf472359e5e54e97f19d2d
Michael Uftring <michael.uftring@gmail.com>
2023-07-12 05:43:51 -0400

    refactor PublishingWorkflow to its own source file in models package

---
+++ server/src/main/scala/com/pennsieve/discover/models/PublishingWorkflow.scala
@@ -0,0 +1,9 @@
+// Copyright (c) 2019 Pennsieve, Inc. All Rights Reserved.
+
+package com.pennsieve.discover.models
+
+object PublishingWorkflow {
+  val Unknown: Long = -1
+  val Version4: Long = 4
+  val Version5: Long = 5
+}
