cb215452e87e250e1dc7dd8bc62f266b651001ed
John Frommeyer <johnfr@seas.upenn.edu>
2024-04-22 14:52:57 -0400

    Update Athena query comment

--- server/src/main/scala/com/pennsieve/discover/clients/AthenaClient.scala
+++ server/src/main/scala/com/pennsieve/discover/clients/AthenaClient.scala
@@ -57,12 +57,15 @@ class AthenaClientImpl(val pennsieveTable: String, val sparcTable: String)
     val escapedTableName = SQLSyntax.createUnsafely(tableName)
     //The discover table in the s3_access_logs_db is a column-for-column ingestion of the s3 access logs
     //An analysis of the logs showed that each full dataset download results in a row in the logs that always
-    // shows operation = 'REST.GET.BUCKET'. The url column of such rows also contain the dataset_id and version
+    // shows operation = 'REST.GET.BUCKET'. The url column of such rows also contain the dataset_id
     // passed as the prefix when it is a full dataset download. We take advantage of this in two places: in the
-    // select block to extract the dataset_id and version, but also in the WHERE block to reject other
+    // select block to extract the dataset_id, but also in the WHERE block to reject other
     // 'REST.GET.BUCKET' operations which are not dataset downloads and do not contain a dataset_id.
     // Likewise, we also remove rows where the requester column contains 's3clean' as those are linked to
     // unpublish actions.
+    // The version is set to zero since it is no longer included in the request_uri.
+    // This is part of a workaround we will use until a different method of tracking direct-from-S3 downloads
+    // is in place.
     sqls"""SELECT regexp_extract(request_uri,'.*prefix=(\d+)(?:%2F|\/).*', 1) AS dataset_id,
          |     0 AS version,
          |     date_parse(requestdatetime, '%d/%b/%Y:%H:%i:%S +%f') as dl_date,
