b2652968d3dffd0db2423ac222c294537faa6d7a
Michael Uftring <michael.uftring@gmail.com>
2025-02-21 13:26:01 -0500

    add AWS Open Data buckets

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -276,6 +276,19 @@ resource "aws_ssm_parameter" "sparc_bucket_role_arn" {
   type  = "String"
   value = data.terraform_remote_state.platform_infrastructure.outputs.sparc_bucket_role_arn
 }
+
+resource "aws_ssm_parameter" "awsod_sparc_publish_50_bucket" {
+  name  = "/${var.environment_name}/${var.service_name}/awsod-sparc-publish-50-bucket"
+  type  = "String"
+  value = data.terraform_remote_state.platform_infrastructure.outputs.awsod_sparc_publish50_bucket_id
+}
+
+resource "aws_ssm_parameter" "awsod_sparc_bucket_role_arn" {
+  name  = "/${var.environment_name}/${var.service_name}/awsod-sparc-bucket-role-arn"
+  type  = "String"
+  value = data.terraform_remote_state.platform_infrastructure.outputs.awsod_sparc_bucket_role_arn
+}
+
 //    RE-JOIN
 resource "aws_ssm_parameter" "rejoin_publish_50_bucket" {
   name  = "/${var.environment_name}/${var.service_name}/rejoin-publish-50-bucket"
