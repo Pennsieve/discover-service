049f13985783e8dea64279c87cc9534b2765e2d7
Rohan Shah <rohanmalavshah@gmail.com>
2026-01-29 11:53:23 -0500

    use task family to incorporate all revisions

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -414,7 +414,7 @@ resource "aws_ssm_parameter" "ecs_s3_storage_cleanup_task_cluster" {
 resource "aws_ssm_parameter" "ecs_s3_storage_cleanup_task_definition" {
   name  = "/${var.environment_name}/${var.service_name}/ecs-s3-storage-cleanup-task-definition"
   type  = "String"
-  value = data.terraform_remote_state.publish_storage_sync.outputs.publish_storage_sync_ecs_task_definition_arn
+  value = data.terraform_remote_state.publish_storage_sync.outputs.publish_storage_sync_ecs_task_definition_family
 }
 
 resource "aws_ssm_parameter" "ecs_s3_storage_cleanup_task_subnets" {
