46e631a10494124c815316886d00e71d3aef4470
ddefreitas.exe <1959720+darrell-d@users.noreply.github.com>
2026-03-17 16:56:30 -0400

    Add Ember storage bucket IAM permissions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -201,6 +201,8 @@ data "aws_iam_policy_document" "iam_policy_document" {
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_publish50_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.precision_embargo50_bucket_arn}/*",
+      data.terraform_remote_state.platform_infrastructure.outputs.ember_pennsieve_storage_bucket_arn,
+      "${data.terraform_remote_state.platform_infrastructure.outputs.ember_pennsieve_storage_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.awsod_sparc_publish50_bucket_arn,
       "${data.terraform_remote_state.platform_infrastructure.outputs.awsod_sparc_publish50_bucket_arn}/*",
       data.terraform_remote_state.platform_infrastructure.outputs.awsod_edots_publish50_bucket_arn,
